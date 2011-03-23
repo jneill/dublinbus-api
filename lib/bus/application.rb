02cf15aa89a0dd6ac5de86f5fa1bef3e6d043064
Jordan Neill <github@jordanneill.com>
2011-03-23 00:47:31 +0000

    making controller methods prettier

--- lib/bus/application.rb
+++ lib/bus/application.rb
@@ -16,24 +16,11 @@ module Bus
     get '/stop/' do
       stops = API.stops
 
-      origin = DefaultOrigin
-
-      if params[:origin]
-      	origin = Geokit::LatLng.normalize(params[:origin])
-        stops = stops.by_distance_from origin
-      end
-
-      if params[:range]
-        stops = stops.within_range(origin, params[:range].to_f)
-      end
-
-      if params[:count]
-      	stops = stops.take(params[:count].to_i)
-      end
-
-      if params[:route]
-        stops = stops.on_route(params[:route])
-      end
+      origin = params[:origin] ? Geokit::LatLng.normalize(params[:origin]) : DefaultOrigin
+      stops = stops.by_distance_from origin if params[:origin]
+      stops = stops.within_range(origin, params[:range].to_f) if params[:range]
+      stops = stops.take(params[:count].to_i) if params[:count]
+      stops = stops.on_route(params[:route]) if params[:route]
 
       result = { :stops => stops.map { |s| s.to_hash.delete_if { |k, v| k == :live } } }
 
@@ -41,27 +28,17 @@ module Bus
     end
 
     get '/stop/:name/' do
-      stops = API.stops.with_name(params[:name])
-
-      not_found if stops.none?
-
-      stops.each(&:update!)
-
-      result = { :stops => stops.map(&:to_hash) }
-
-      jsonp result
+      live_data API.stops.with_name params[:name]
     end
 
     get '/stop/:name/:id' do
-      stops = API.stops.with_id(params[:id])
-
-      not_found if stops.none?
-
-      stops.each(&:update!)
-
-      result = { :stops => stops.map(&:to_hash) }
-
-      jsonp result
+      live_data API.stops.with_id params[:id]
     end
+
+    def live_data(stops)
+      not_found if stops.none?
+      stops.each(&:update!)
+      jsonp({ :stops => stops.map(&:to_hash) })
+	end
   end
 end
